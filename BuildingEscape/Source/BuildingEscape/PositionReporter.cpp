544fb472c27575b871f8af3ef5ad19a3fc27f723
sedratycoon2 <sedratycoon2@gmail.com>
2018-02-19 17:31:44 -0400

    Played around with UE_LOG messages
    Added it to two static components and got their names printed in the log.

--- BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReporter.h"
+#include "Gameframework/Actor.h"
 
 
 // Sets default values for this component's properties
@@ -19,7 +20,8 @@ void UPositionReporter::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	FString ObjectName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("PositionReporter now attached to %s"), *ObjectName);
 	
 }
 
